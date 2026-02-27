465e8ac36da749d4e5482d08f6ebe23fe603bd63
Alp Efe Şekerci <alpefesekerci@gmail.com>
2026-02-27 11:19:45 +0300

    for döngüsü
    scanner değişkeni başa eklendi.

--- src/studentManageApp/Main.java
+++ src/studentManageApp/Main.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Main {
     static void main() {
 
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Kaç Öğrenci Gireceksiniz? ");
         int ogrenciSayisi = scanner.nextInt();
         scanner.nextLine();
@@ -12,7 +14,17 @@ public class Main {
         String isimler[] = new String[ogrenciSayisi];
         int notlar[] = new int[ogrenciSayisi];
 
-        
+        for (int i = 0; i < ogrenciSayisi; i++){
+            System.out.println((i+1) + ". öğrencinin adını giriniz: ");
+            isimler[i] = scanner.nextLine();
+
+            System.out.println((i+1) + ". öğrencinin notunu giriniz: ");
+            notlar[i] = scanner.nextInt();
+            scanner.nextLine();
+        }
+
+        System.out.println("\nVeri Girişi Tamamlandı!");
+
 
 
 
