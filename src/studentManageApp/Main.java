0cdb7fa9e6adb7413d8b5d006d0db43c5c30349c
Alp Efe Şekerci <alpefesekerci@gmail.com>
2026-02-27 11:05:30 +0300

    scanner ekleme / öğrenci sayısını ekleme

--- src/studentManageApp/Main.java
+++ src/studentManageApp/Main.java
@@ -5,5 +5,13 @@ import java.util.Scanner;
 public class Main {
     static void main() {
 
+        System.out.println("Kaç Öğrenci Gireceksiniz? ");
+        int ogrenciSayisi = scanner.nextInt();
+        scanner.nextLine();
+
+        
+
+
+
     }
 }
